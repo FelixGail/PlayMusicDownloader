94e27e82e917d42caecdf6576633e99ce1bc298c
Felix Gail <felix.clemens.gail@gmail.com>
2020-08-05 13:14:07 +0200

    fix relative paths

--- m3u_creator.py
+++ m3u_creator.py
@@ -11,17 +11,19 @@ EasyID3.RegisterTXXXKey('playlists', 'Google Play Playlist')
 def main():
     for root, subdirs, files in os.walk(config.get_song_path()):
         for file in files:
-            print(file + ":")
+            relpath = os.path.relpath(root, config.get_song_path())
+            rel_filepath = os.path.join(relpath, file)
+            print(rel_filepath + ":")
             try:
                 meta = EasyID3(os.path.join(root, file))
                 if 'playlists' in meta:
                     for playlist in meta['playlists']:
                         print("\t->{}".format(playlist))
-                        with open(os.path.join(root, remove_forbidden_characters(playlist) + ".m3u"), 'a+') as p_file:
+                        with open(os.path.join(config.get_song_path(), remove_forbidden_characters(playlist) + ".m3u"), 'a+') as p_file:
                             p_file.seek(0)
                             lines = p_file.read().splitlines()
-                            if str(file) not in lines:
-                                p_file.write(file)
+                            if str(file) not in [elem.split('/')[-1] for elem in lines]:
+                                p_file.write(rel_filepath)
                                 p_file.write("\n")
                                 print("\t\t-> writing to file")
                             else:
